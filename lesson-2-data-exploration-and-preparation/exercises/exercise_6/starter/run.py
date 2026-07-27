f3dd99a525dcacb91bec00b6fdff931cdc33ed2b
Marcella <marcellalharris@yahoo.com>
2026-07-27 15:30:49 -0400

    exercise 3.3: implement stratified train/test split, pin wandb>=0.28.0

--- lesson-2-data-exploration-and-preparation/exercises/exercise_6/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_6/starter/run.py
@@ -46,8 +46,18 @@ def go(args):
     ###################################
     # COMPLETE the following line     #
     ###################################
+    # Step 1: Data Segregation. Complete an MLflow to divide the data into train/test sets.
 
-    splits["train"], splits["test"] = # USE train_test_split here to split df according to the provided args.test_size
+    #== process message ==
+    logger.info("Split the data according to the provided args.test_size")
+
+    #== train & test split each class ==
+    splits["train"], splits["test"] = train_test_split(
+        df,
+        test_size = args.test_size,
+        random_state = args.random_state,
+        stratify = df[args.stratify] if args.stratify != "null" else None
+    )
 
     # Now we save the artifacts. We use a temporary directory so we do not leave
     # any trace behind
