8cfd2b0204bd3764e0af059be7a6f88b64abd537
Marcella <marcellalharris@yahoo.com>
2026-08-25 14:34:34 -0400

    exercise 5.3: build a component that fetches a model and test it on the test dataset

--- lesson-4-training-validation-experiment-tracking/exercises/exercise_13/starter/run.py
+++ lesson-4-training-validation-experiment-tracking/exercises/exercise_13/starter/run.py
@@ -17,7 +17,10 @@ def go(args):
 
     logger.info("Downloading and reading test artifact")
     ## Get the args.test_data artifact from W&B locally
-    test_data_path = ## YOUR CODE HERE
+
+    #== test the data argument ==
+    test_data_path = run.use_artifact(args.test_data).file()
+
     df = pd.read_csv(test_data_path, low_memory=False)
 
     # Extract the target from the features
@@ -29,13 +32,16 @@ def go(args):
 
     ## Get the args.model_export artifact from W&B locally. Since this artifact contains a directory
     # and not a single file, you will have to use .download() instead of .file()
-    model_export_path = ## YOUR CODE HERE
+    #== download the model ==
+    model_export_path = run.use_artifact(args.model_export).download()
 
     # Load the model using mlflow.sklearn.load_model
-    pipe = ## YOUR CODE HERE
+    #== load the model ==
+    pipe = mlflow.sklearn.load_model(model_export_path)
 
     # Compute the prediction from the model using .predict_proba on the test set
-    pred_proba = ## YOUR CODE HERE
+    #== compute the prediction ==
+    pred_proba = pipe.predict_proba(X_test)
 
     logger.info("Scoring")
     score = roc_auc_score(y_test, pred_proba, average="macro", multi_class="ovo")
